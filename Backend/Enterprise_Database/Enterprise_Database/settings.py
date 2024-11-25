7935025307b886e79140b2c8f7c8fc8bd31fea96
Manav Dhruve <manavdhruve7@gmail.com>
2024-11-25 01:40:56 -0500

    adding login api

--- Backend/Enterprise_Database/Enterprise_Database/settings.py
+++ Backend/Enterprise_Database/Enterprise_Database/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base','rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -129,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
