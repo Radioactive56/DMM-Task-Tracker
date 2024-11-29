f60819f48d29180f04a4a6305c72d1b75a7df359
Manav Dhruve <manavdhruve7@gmail.com>
2024-11-29 04:32:45 -0500

    Home page

--- Frontend/enterprise_frontend/src/App.js
+++ Frontend/enterprise_frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Form from './components/Form'
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 export let API_URL = process.env.REACT_APP_API_URL
 
@@ -13,8 +14,8 @@ function App() {
     <Router>
       <Routes>
         <Route exact path='/' element={<Login></Login>}></Route>
-        <Route exact path='/project' element={<Form></Form>}>
-        </Route>
+        <Route exact path='/project' element={<Form></Form>}></Route>
+        <Route exact path='/home' element={<Dashboard></Dashboard>}></Route>
       </Routes>
     </Router>
   );
