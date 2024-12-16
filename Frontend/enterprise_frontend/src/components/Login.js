866d6574c4a9372efcbbaff197f5cd644e36444f
Radioactive56 <manavdhruve7@gmail.com>
2024-12-16 05:34:51 -0500

    Updating ui and jwt

--- Frontend/enterprise_frontend/src/components/Login.js
+++ Frontend/enterprise_frontend/src/components/Login.js
@@ -10,7 +10,7 @@ import Cookies from 'js-cookie';
 export default function Login() {
     const [captchaUrl, setCaptchaUrl] = useState(""); // URL to fetch captcha image
     const navigate = useNavigate();
-    const { register, handleSubmit, reset,setError, clearErrors, formState: { errors } } = useForm();
+    const { register, handleSubmit, clearErrors, formState: { errors } } = useForm();
 
     const fetchCaptcha = () => {
     setCaptchaUrl(`${API_URL}/generate-captcha/?t=${new Date().getTime()}`); // Cache-busting parameter
@@ -52,7 +52,7 @@ export default function Login() {
             })
             .then(data=>{
                 console.log(data)
-                Cookies.set('Token',data,{expires:30/1440})
+                Cookies.set('Token',data.Token,{expires:30/1440})
                 navigate('/home')
 
             })
@@ -71,7 +71,7 @@ export default function Login() {
        
        
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-            <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to ISAE</h5>
+            <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to DMM Server</h5>
             <div>
                 <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Username</label>
                 <input {...register('username')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="username" required />
---
+++ Frontend/enterprise_frontend/src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import Dropdown from './Dropdown';
+import Cookies from 'js-cookie'
+import SidebarNew from './SidebarNew';
+
+
+
+export default function Navbar() {
+    const navigate = useNavigate();  
+    const logout =()=>{
+        Cookies.remove('Token')
+        localStorage.clear()
+        navigate('/')
+    }
+     const name = localStorage.getItem("name")
+  
+      const navbar = {
+          background : "linear-gradient(90deg,#007991 0% ,#78ffd6 100%)",
+          color: "antiquewhite"
+          };
+        return(
+          
+      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <div className="px-3 py-3 lg:px-5 lg:pl-3" style={navbar}>
+        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-start rtl:justify-end">
+            <SidebarNew/>
+        </div>
+            <Link to="/dashboard" className="flex ms-2 md:me-24">
+              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">DMM SERVER</span>
+            </Link>
+            
+          <div className="flex items-center">
+              <div className="flex items-center ms-3">
+              <Dropdown onSignOut={logout}>
+              </Dropdown>
+            </div>
+        </div>
+      </div>
+      </div>
+      </nav>
+        );
+}
