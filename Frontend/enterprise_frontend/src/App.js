8a7f887e3479674aa6ce23b843e0755824c6c49d
Radioactive56 <manavdhruve7@gmail.com>
2024-12-18 02:59:38 -0500

    update form and fetching projects completed

--- Frontend/enterprise_frontend/src/App.js
+++ Frontend/enterprise_frontend/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 import Projects from './components/Projects';
+import ProjectUpdateForm from './components/ProjectUpdateForm'
 
 export let API_URL = process.env.REACT_APP_API_URL
 
@@ -18,6 +19,7 @@ function App() {
         <Route exact path='/' element={<Login></Login>}></Route>
         <Route exact path='/project' element={<PrivateRoute><Form></Form></PrivateRoute>}></Route>
         <Route exact path='/projects' element={<PrivateRoute><Projects/></PrivateRoute>}></Route>
+        <Route exact path='/projects/update/:id' element={<PrivateRoute><ProjectUpdateForm/></PrivateRoute>}></Route>
         <Route exact path='/home' element={<PrivateRoute><Dashboard></Dashboard></PrivateRoute>}></Route>
       </Routes>
     </Router>
